167fce8296b40d19854266278250edcc813f7bf7
Shinobu-warrior <yourdost2004@gmail.com>
2023-11-21 10:09:19 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,11 @@
+formel=document.getElementById("formel");
+btn=document.getElementsByClassName("btn");
+
+
+function displayValue() {
+    // Replace the text inside the "display" paragraph with the desired value
+    displayElement.innerText += "";
+}
+
+// Attach the function to the button's click event
+button.addEventListener("click", displayValue);
